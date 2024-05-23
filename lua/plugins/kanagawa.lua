f2062ef89c26e8cddffc9af056f622a13e758078
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2024-05-22 21:31:34 -0600

    kanagawa theme

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,14 @@
+return {
+  'rebelot/kanagawa.nvim',
+  name = 'kanagawa',
+  opts = {
+    undercurl = true,
+    statementStyle = { bold = false },
+    -- commentStyle = { italic = true},
+    -- keywordStyle = { italic = true },
+    theme = "wave",
+    background = {
+      dark = "wave"
+    }
+  }
+}
